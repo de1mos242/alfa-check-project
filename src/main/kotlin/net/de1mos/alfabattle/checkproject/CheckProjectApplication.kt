4947fd37fc9bc57d739c4a41681b88d2ecd426e5
Denis Yakovlev <de1m0s242@gmail.com>
2020-06-26 23:41:34 +0700

    Initial commit

---
+++ src/main/kotlin/net/de1mos/alfabattle/checkproject/CheckProjectApplication.kt
@@ -0,0 +1,11 @@
+package net.de1mos.alfabattle.checkproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CheckProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<CheckProjectApplication>(*args)
+}
